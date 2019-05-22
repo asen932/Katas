f6b72d9d503869af92fdff7a53fd7687b81f9035
Asen <asen@yowa.eu>
2019-05-22 23:59:03 +0300

    initial

---
+++ src/oddCount.java
@@ -0,0 +1,20 @@
+public class oddCount {
+
+    public static int oddCount(int n){
+
+        int counter = 0;
+
+        if(n > 0){
+            for (int i = 0; i < n; i++){
+                if (i % 2 != 0){
+                    counter++;
+                }
+            }
+            return counter;
+        }
+        else{
+            return 0;
+        }
+    }
+
+}
